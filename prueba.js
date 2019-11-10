6ebb4ce07f207ef3cf609a1bf3a3763971a2ce43
Dani Vicario <danivicario@dvicario.es>
2019-11-10 09:56:12 +0100

    added

--- prueba.js
+++ prueba.js
@@ -54,9 +54,23 @@ const PI_HALF = Math.PI / 2;
 canvasDOMEl.setAttribute("height", window.innerHeight);
 canvasDOMEl.setAttribute("width", window.innerWidth);
 
-function makeMatrix(e, size, gap, xxx) {
-  ctx.save();
-  ctx.translate(w2 - (e * gap) / 2, h2 - (e * gap) / 2);
+function randomFloat(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// eslint-disable-next-line no-unused-vars
+const shuffle = (array) => array.sort(() => Math.random() - 0.5);
+
+// eslint-disable-next-line no-unused-vars
+const randomArrayPosition = (array) => array[randomInt(0, array.length - 1)];
+
+// ctx.globalCompositeOperation = globalCompositeOperationModes.difference;
+
+function getDataModel(e, size, gap) {
   const circles = [];
   let i = 0;
 
@@ -64,30 +78,49 @@ function makeMatrix(e, size, gap, xxx) {
     if (!(e % 2)) i++;
     for (let row = 0; row < e; row += 1) {
       i++;
-      circles.push([row * gap, column * gap, !(i % 2) ? "#ff0000ff" : "#000000ff"]);
+      circles.push({
+        row: row * gap,
+        col: column * gap,
+        color: randomArrayPosition(["#0d0208", "#003b00", "#008f11", "#00ff41"]), // i % 2 ? "#ff0000ff" : "#000000ff", //`#${((Math.random() * 0xffffff) << 0).toString(16)}`,
+        fType: randomInt(1, 3)
+      });
     }
   }
-
+  return circles;
+}
+function makeMatrix(circles, size, xxx) {
   circles.forEach((x, idx) => {
+    let fn;
+
+    if (x.fType === 1) fn = Math.tan;
+    if (x.fType === 2) fn = Math.cos;
+    if (x.fType === 3) fn = Math.cos;
+
     ctx.beginPath();
-    ctx.fillStyle = `${x[2]}`;
-    ctx.arc(
-      x[0] + size,
-      x[1] + size,
-      Math.abs(size * Math.sin(((idx / 45) * Math.PI) / 180)) * xxx,
-      0,
-      PI_DOUBLE
-    );
-    // ctx.fill();
-    ctx.stroke();
+    ctx.fillStyle = `${x.color}`;
+    let r = Math.abs(size * fn((360 * Math.PI) / 180)) * xxx + randomFloat(0.1, 1.9);
+    // r *= ;
+    ctx.arc(x.row, x.col, r, 0, PI_DOUBLE);
+    ctx.fill();
+    // ctx.lineWidth = 0.5;
+    // ctx.stroke();
     ctx.closePath();
   });
-  ctx.restore();
 }
 
 let xxx = 1;
-setInterval(() => {
-  xxx += 0.01;
+const e = 100;
+const size = 5;
+const gap = 20;
+const circles = getDataModel(e, size, gap, xxx);
+
+let intervalID = setInterval(() => {
   ctx.clearRect(0, 0, w, h);
-  makeMatrix(60, 10, 30, xxx);
+  ctx.save();
+  ctx.translate(w2 - (e * gap) / 2, h2 - (e * gap) / 2);
+  // xxx += 0.005;
+  makeMatrix(circles, size, xxx);
+  ctx.restore();
+
+  if (xxx > 30) clearInterval(intervalID);
 }, 10);
